649d35ba40a3e281c89995946b62061b798e6126
Jillur Rahman <developerjillur@gmail.com>
2024-08-16 23:45:17 +0600

    Update jet-smart-filters-search-by-taxonomy-term.php
    Added Meta Fields support v2

--- jet-smart-filters-search-by-taxonomy-term.php
+++ jet-smart-filters-search-by-taxonomy-term.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: JetSmartFilters - Search Posts by Taxonomy Term
+ * Plugin Name: JetSmartFilters - Search by Taxonomy Term & Meta Data
  * Plugin URI:  https://crocoblock.com/freemium/tools/
- * Description: Extend JetSmartFilters to search by custom taxonomy term names. Have option to provied one or multiple taxonomy slugs under on Setting->JSF Taxonomy Search.
- * Version:     1.1.0
+ * Description: Extend JetSmartFilters to search by custom taxonomy term names or Meta Fields also. Have the option to provide one or multiple taxonomy slugs and meta Fields under Setting->JSF Taxonomy Search.
+ * Version:     2.1.0
  * Author:      Developer Jillur
  * Author URI:  https://crocoblock.com/
  * Requires Plugins:  JetEngine, JetSmartFilters
@@ -32,35 +32,52 @@ add_action( 'admin_menu', 'jsf_sbtax_add_admin_menu' );
 add_action( 'admin_init', 'jsf_sbtax_settings_init' );
 
 function jsf_sbtax_add_admin_menu() { 
-	add_options_page( 'JetSmartFilters - Taxonomy Term Search', 'JSF Taxonomy Search', 'manage_options', 'jsf_taxonomy_search', 'jsf_sbtax_options_page' );
-}
-
-function jsf_sbtax_settings_init() { 
-	register_setting( 'pluginPage', 'jsf_sbtax_settings' );
-
-	add_settings_section(
-		'jsf_sbtax_pluginPage_section', 
-		__( 'Settings for JetSmartFilters - Taxonomy Term Search', 'jsf_sbtax' ), 
-		null, 
-		'pluginPage'
-	);
-
-	add_settings_field( 
-		'jsf_sbtax_taxonomy_slug', 
-		__( 'Custom Taxonomy Slug', 'jsf_sbtax' ), 
-		'jsf_sbtax_taxonomy_slug_render', 
-		'pluginPage', 
-		'jsf_sbtax_pluginPage_section' 
-	);
+	add_options_page( 'JetSmartFilters - Taxonomy Term Search 2.0', 'JSF Taxonomy Search', 'manage_options', 'jsf_taxonomy_search', 'jsf_sbtax_options_page' );
 }
 
 // In jet-smart-filters-search-by-taxonomy-term.php
+function jsf_sbtax_settings_init() { 
+    register_setting( 'pluginPage', 'jsf_sbtax_settings' );
+
+    add_settings_section(
+        'jsf_sbtax_pluginPage_section', 
+        __( 'Settings for JetSmartFilters - Taxonomy Term & Meta Search', 'jsf_sbtax' ), 
+        null, 
+        'pluginPage'
+    );
+
+    add_settings_field( 
+        'jsf_sbtax_taxonomy_slug', 
+        __( 'Custom Taxonomy Slugs', 'jsf_sbtax' ), 
+        'jsf_sbtax_taxonomy_slug_render', 
+        'pluginPage', 
+        'jsf_sbtax_pluginPage_section' 
+    );
+
+    add_settings_field( 
+        'jsf_sbtax_meta_keys', 
+        __( 'Custom Meta Keys', 'jsf_sbtax' ), 
+        'jsf_sbtax_meta_keys_render', 
+        'pluginPage', 
+        'jsf_sbtax_pluginPage_section' 
+    );
+}
+
 function jsf_sbtax_taxonomy_slug_render() { 
     $options = get_option( 'jsf_sbtax_settings' );
-    $taxonomy_slugs = isset( $options['jsf_sbtax_taxonomy_slug'] ) ? $options['jsf_sbtax_taxonomy_slug'] : '';
+    $taxonomy_slug = isset( $options['jsf_sbtax_taxonomy_slug'] ) ? $options['jsf_sbtax_taxonomy_slug'] : '';
     ?>
-    <input type='text' class='regular-text' name='jsf_sbtax_settings[jsf_sbtax_taxonomy_slug]' value='<?php echo esc_attr( $taxonomy_slugs ); ?>'>
-    <p class="description"><?php _e( 'Enter taxonomy slugs separated by commas (e.g., category,post_tag,custom_taxonomy).', 'jsf_sbtax' ); ?></p>
+    <input type='text' name='jsf_sbtax_settings[jsf_sbtax_taxonomy_slug]' value='<?php echo esc_attr( $taxonomy_slug ); ?>'>
+    <p class="description"><?php _e( 'Enter custom taxonomy slugs separated by commas (e.g., taxonomy1,taxonomy2).', 'jsf_sbtax' ); ?></p>
+    <?php
+}
+
+function jsf_sbtax_meta_keys_render() { 
+    $options = get_option( 'jsf_sbtax_settings' );
+    $meta_keys = isset( $options['jsf_sbtax_meta_keys'] ) ? $options['jsf_sbtax_meta_keys'] : '';
+    ?>
+    <input type='text' name='jsf_sbtax_settings[jsf_sbtax_meta_keys]' value='<?php echo esc_attr( $meta_keys ); ?>'>
+    <p class="description"><?php _e( 'Enter custom meta keys separated by commas (e.g., meta_key1,meta_key2).', 'jsf_sbtax' ); ?></p>
     <?php
 }
 
